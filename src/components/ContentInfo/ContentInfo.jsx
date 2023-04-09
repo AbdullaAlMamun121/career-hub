61152486ce4e7cfaa4fc7d35eae9c60bf1221d41
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-04-09 23:10:01 +0600

    try to add some basic folder structure

---
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContentInfo = () => {
+    return (
+        <div>
+            <h3>All content will be here</h3>
+        </div>
+    );
+};
+
+export default ContentInfo;
