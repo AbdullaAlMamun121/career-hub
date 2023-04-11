b6411ff4624628002f28fc2d5430eca7071faac2
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-04-11 18:10:46 +0600

    header image changed

--- src/components/HeaderBodyOne/HeaderBodyOne.jsx
+++ src/components/HeaderBodyOne/HeaderBodyOne.jsx
@@ -4,13 +4,13 @@ const HeaderBodyOne = () => {
     return (
         <div>
             <header className="header-body">
-                <div className="header-section text-section">
+                <div className="header-section text-section mt-5">
                     <h1>One Step Closer To Your <span className='dream-text'>Dream Job</span></h1>
                     <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
                     <button className='get-start'>Get started</button>
                 </div>
                 <div className="header-section image-section">
-                    <img src="https://as2.ftcdn.net/v2/jpg/04/23/15/69/1000_F_423156976_nrYxW2nD8KoQSIdRtyg0fbLmD6q4pNTT.jpg" alt="banner" />
+                    <img src="All-Images/P3OLGJ1 copy 1.png" alt="banner" />
                 </div>
             </header>
         </div>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import HeaderBodyTwo from '../HeaderBodyTwo/HeaderBodyTwo';
 
-
 const Statistics = () => {
     const data = [
         {
